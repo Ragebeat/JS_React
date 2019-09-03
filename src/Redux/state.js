bf06dc93d95ee6cd1d728b47df3fc804e0be7f2a
Ragebeat <ragebeat555@gmail.com>
2019-09-03 22:30:30 +0900

    03.09.19 last save

---
+++ src/Redux/state.js
@@ -0,0 +1,26 @@
+import App from "../App";
+
+let state = {
+    profilePage :{
+    posts: [
+        {id: 1, message: "Hellow everyone", likes: 15},
+        {id: 2, message: "It's my first post", likes: 20},
+        {id: 3, message: "Yohohoh", likes: 2},
+        {id: 4, message: "Nya", likes: 200}
+    ]},
+    dialogsPage :{
+    dialogs: [
+        {id: 1, message: "Hello!"},
+        {id: 2, message: "How are You?"},
+        {id: 3, message: ":):):)"}
+    ],
+    dialogsItems: [
+        {id: 1, name: "Sveta"},
+        {id: 2, name: "Masha"},
+        {id: 3, name: "Yura"},
+        {id: 4, name: "Misha"},
+        {id: 5, name: "Karl"},
+        {id: 6, name: "Yohohohh"}
+    ]}
+}
+export default state;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,8 @@ import DialogsItem from "./DialogsItem/DialogsItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-    let dialogsItemsElements = props.dialogsItems.map(d => <DialogsItem name={d.name} id={d.id} key={d.id}/>);
-    let dialogsElements = props.dialogs.map(m => <Message message={m.message}  key={m.id}/>);
+    let dialogsItemsElements = props.state.dialogsItems.map(d => <DialogsItem name={d.name} id={d.id} key={d.id}/>);
+    let dialogsElements = props.state.dialogs.map(m => <Message message={m.message}  key={m.id}/>);
 
     return (
         <div className={s.dialogs}>
