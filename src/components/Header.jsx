049422e583220731d615ce13a5dab4cef100f0c7
Ragebeat <ragebeat555@gmail.com>
2019-09-03 12:28:16 +0900

    paste project in git folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (<header className="header">
+        <img src="https://png.pngtree.com/element_pic/16/11/02/bd886d7ccc6f8dd8db17e841233c9656.jpg"
+             alt="bd886d7ccc6f8dd8db17e841233c9656"></img>
+    </header>);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (<div className="content">
+        <div>
+            <img src="https://media.wired.com/photos/5a593a7ff11e325008172bc2/master/pass/pulsar-831502910.jpg"
+                 width="100%" alt="pulsar"></img>
+        </div>
+        <div>
+            ava + desc
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    </div>);
+}
+
+export default Profile;
